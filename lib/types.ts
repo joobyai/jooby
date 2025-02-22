6098f95da6dc3c149d817c5585dc4fa9f8c622bd
joobyai <cgo7up@gmail.com>
2025-02-22 23:29:33 +0100

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -27,35 +27,6 @@ export interface TranslationContent {
   startChat: string;
 }
 
-export interface TranslationContent {
-  context: string;
-  typing: string;
-  userIdentifier: string;
-  userChoice: string;
-  title: string;
-  welcome: string;
-  question: string;
-  onlineJob: string;
-  localJob: string;
-  placeholder: string;
-  send: string;
-  disclaimer: string;
-  budgetQuestion: string;
-  countryQuestion: string;
-  emailQuestion: string;
-  phoneQuestion: string;
-  skillsQuestion: string;
-  footerDisclaimer: string;
-  successMessage: string;
-  nameQuestion: string;
-  sectorQuestion: string;
-  jobTypeQuestion: string;
-  experienceQuestion: string;
-  availabilityQuestion: string;
-  finalMessage: string;
-  startChat: string;
-}
-
 interface Translations {
   en: TranslationContent;
   fr: TranslationContent;
