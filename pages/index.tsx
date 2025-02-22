f2f02522b541cac8d164b464fd4f898ed5b885e4
joobyai <cgo7up@gmail.com>
2025-02-22 22:58:19 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import localeData from "../lib/locale";
-import type { TranslationContent } from './types';
+//import type { TranslationContent } from './types';
+import type { TranslationContent } from "../lib/types";
 
 interface ChatMessage {
   role: "user" | "assistant" | "system";
